0f053a9b652df0b7b71c330abf440faea8977331
Jenna Ransom <jenna.ransom@gmail.com>
2020-04-19 12:51:18 -0600

    add message fetch and user form, test both in console

--- stranger_messages_frontend/src/index.js
+++ stranger_messages_frontend/src/index.js
@@ -1,15 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  
   // needed variables
-    // messages container 
-    // username form 
-    // message form 
-    // base url for fetch calls
+    const msg_container = document.querySelector('#message_container')
+    const uform = document.querySelector('#user_form') 
+    const mform = document.querySelector('#message_form') 
+    const base_url = "http://localhost:3000"
 
   // fetch call for messages
+  fetch(base_url + '/messages')
+    .then(res => res.json())
+    .then(json => console.log(json))
+
+  // const users = []
 
   // event lisenters 
-    // user form submit 
+  uform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let uname = document.querySelector('#username').value
+    console.log(uname)
+    // function to create a user
+  })
     // message form submit 
     // play message 
 
