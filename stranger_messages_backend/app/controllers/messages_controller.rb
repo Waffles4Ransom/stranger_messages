3370843b0b10e8d7862be9cb728076c49e7fed37
Jenna Ransom <jenna.ransom@gmail.com>
2020-04-22 16:28:24 -0600

    add more actions to user and message controllers

--- stranger_messages_backend/app/controllers/messages_controller.rb
+++ stranger_messages_backend/app/controllers/messages_controller.rb
@@ -1,6 +1,32 @@
 class MessagesController < ApplicationController 
+
   def index 
-    @messages = Message.all 
-    render json: @messages
+    messages = Message.all 
+    render json: messages.to_json(:include => { :user => {:only => [:id, :username]}}, :except => [:created_at, :updated_at])
   end 
+
+  def show 
+    message = Message.find(params[:id])
+    if message 
+      render json: message, except: [:created_at, :updated_at]
+    else 
+      render json: { message: "Message not found"}
+    end 
+  end 
+
+  def create 
+    message = Message.new(message_params)
+    if message.save 
+      render json: message, except: [:created_at, :updated_at]
+    else 
+      render json: { errors: message.errors}
+    end 
+  end 
+
+  private 
+
+  def message_params
+    params.require(:message).permit(:name, :content, :user_id)
+  end   
+
 end 
--- stranger_messages_backend/app/controllers/users_controller.rb
+++ stranger_messages_backend/app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
 
   def index 
-    @users = User.all 
-    render json: @users
+    users = User.all 
+    render json: users, except: [:created_at, :updated_at]
+  end 
+
+  def create 
+    user = User.new(user_params)
+    if @user.save 
+      render json: user
+    else
+      render json: errors: user.errors
+    end  
+  end 
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:username)
   end 
 
 end 
